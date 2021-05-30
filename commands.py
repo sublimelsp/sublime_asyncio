6b174fcce02bd42677143ada81c0d26a295b1a12
Raoul Wols <raoulwols@gmail.com>
2021-05-30 22:49:34 +0200

    remove 3 commands

--- commands.py
+++ commands.py
@@ -42,51 +42,11 @@ class DispatchMixin:
         run_coroutine(wrap())
 
 
+
 class ApplicationCommand(sublime_plugin.ApplicationCommand, DispatchMixin):
     """
     An asynchronous ApplicationCommand. Override the `execute` method instead
     of the `run` method.
-
-    This is the exclusive task variant. Meaning, if you run this command, and
-    then run it again, the first invocation (if it's still running) will be
-    cancelled. Only one `execute` call is running at all times.
-    """
-    def __init__(self):
-        super().__init__()
-        DispatchMixin.__init__(self)
-        self.__task: Optional[ref[Task]] = None
-
-    def __del__(self) -> None:
-        self.__cancel()
-        super().__del__()
-
-    def __cancel(self) -> None:
-        if self.__task:
-            if task := self.__task():
-                call_soon_threadsafe(task.cancel)
-            self.__task = None
-
-    def __store_task(self, task: Task) -> None:
-        self.__task = ref(task)
-
-    def run(self, **kwargs: Any) -> None:  # type: ignore
-        self.__cancel()
-        run_coroutine(self.execute(**kwargs), self.__store_task)
-
-    async def execute(self, **kwargs: Any) -> None:
-        """
-        The asynchronous entrypoint of your command.
-        """
-        pass
-
-
-class ConcurrentApplicationCommand(sublime_plugin.ApplicationCommand, DispatchMixin):
-    """
-    An asynchronous ApplicationCommand. Override the `execute` method instead
-    of the `run` method.
-
-    This is the concurrent task variant. Meaning, if you run this command, and
-    then run it again, the first invocation will keep running.
     """
     def run(self, **kwargs: Any) -> None:  # type: ignore
         self.dispatch(self.execute(**kwargs))
@@ -102,47 +62,6 @@ class WindowCommand(sublime_plugin.WindowCommand, DispatchMixin):
     """
     An asynchronous WindowCommand. Override the `execute` method instead
     of the `run` method.
-
-    This is the exclusive task variant. Meaning, if you run this command, and
-    then run it again, the first invocation (if it's still running) will be
-    cancelled. Only one `execute` call is running at all times.
-    """
-    def __init__(self, window: sublime.Window):
-        super().__init__(window)
-        DispatchMixin.__init__(self)
-        self.__task: Optional[ref[Task]] = None
-
-    def __del__(self) -> None:
-        self.__cancel()
-        super().__del__()
-
-    def __cancel(self) -> None:
-        if self.__task:
-            if task := self.__task():
-                call_soon_threadsafe(task.cancel)
-            self.__task = None
-
-    def __store_task(self, task: Task) -> None:
-        self.__task = ref(task)
-
-    def run(self, **kwargs: Any) -> None:  # type: ignore
-        self.__cancel()
-        run_coroutine(self.execute(**kwargs), self.__store_task)
-
-    async def execute(self, **kwargs: Any) -> None:
-        """
-        The asynchronous entrypoint of your command.
-        """
-        pass
-
-
-class ConcurrentWindowCommand(sublime_plugin.WindowCommand, DispatchMixin):
-    """
-    An asynchronous WindowCommand. Override the `execute` method instead
-    of the `run` method.
-
-    This is the concurrent task variant. Meaning, if you run this command, and
-    then run it again, the first invocation will keep running.
     """
     def __init__(self, window: sublime.Window):
         super().__init__(window)
@@ -163,46 +82,6 @@ class ViewCommand(sublime_plugin.TextCommand, DispatchMixin):
     An asynchronous ViewCommand. Override the `execute` method instead
     of the `run` method.
 
-    This is the exclusive task variant. Meaning, if you run this command, and
-    then run it again, the first invocation (if it's still running) will be
-    cancelled. Only one `execute` call is running at all times.
-    """
-    def __init__(self, view: sublime.View):
-        super().__init__(view)
-        DispatchMixin.__init__(self)
-        self.__task: Optional[ref[Task]] = None
-
-    def __del__(self) -> None:
-        self.__cancel()
-        super().__del__()
-
-    def __cancel(self) -> None:
-        if self.__task:
-            if task := self.__task():
-                call_soon_threadsafe(task.cancel)
-            self.__task = None
-
-    def __store_task(self, task: Task) -> None:
-        self.__task = ref(task)
-
-    def run(self, **kwargs: Any) -> None:  # type: ignore
-        # async view commands cannot mutate the buffer
-        kwargs.pop("edit")
-        self.__cancel()
-        run_coroutine(self.execute(**kwargs), self.__store_task)
-
-    async def execute(self, **kwargs: Any) -> None:
-        """
-        The asynchronous entrypoint of your command.
-        """
-        pass
-
-
-class ConcurrentViewCommand(sublime_plugin.TextCommand, DispatchMixin):
-    """
-    An asynchronous ViewCommand. Override the `execute` method instead
-    of the `run` method.
-
     This is the concurrent task variant. Meaning, if you run this command, and
     then run it again, the first invocation will keep running.
     """
@@ -210,9 +89,7 @@ class ConcurrentViewCommand(sublime_plugin.TextCommand, DispatchMixin):
         super().__init__(view)
         DispatchMixin.__init__(self)
 
-    def run(self, **kwargs: Any) -> None:  # type: ignore
-        # async view commands cannot mutate the buffer
-        kwargs.pop("edit")
+    def run(self, _: sublime.Edit, **kwargs: Any) -> None:  # type: ignore
         self.dispatch(self.execute(**kwargs))
 
     async def execute(self, **kwargs: Any) -> None:
